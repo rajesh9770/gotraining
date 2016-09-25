e4117a1eb13daa064c80de8fe2110d185760c940
rajesh9770 <rajesh_9770@yahoo.com>
2016-09-25 16:56:23 -0700

    print binary, hex

--- src/HelloWorld/first.go
+++ src/HelloWorld/first.go
@@ -6,4 +6,8 @@ import (
 
 func main() {
 	fmt.Println("Hello world Test!")
+	fmt.Printf("%d %b %o %#x", 42, 42, 42, 42)
+	for i:=1; i<20000; i++ {
+		fmt.Printf("%d %b %o %#x UTF-8 %q\n", i, i, i, i, i)
+	}
 }
