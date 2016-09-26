52539bb4f3a8c998bda0e8e8387a05386536e40c
rajesh9770 <rajesh_9770@yahoo.com>
2016-09-25 19:41:19 -0700

    variables

--- src/main/main.go
+++ src/main/main.go
@@ -1,10 +1,10 @@
-package HelloWorld 
+package main
 
 import (
 	"fmt"
 )
 
-func test() {
+func main() {
 //	fmt.Println("Hello world Test!")
 //	fmt.Printf("%d %b %o %#x", 42, 42, 42, 42)
 //	for i:=1; i<20000; i++ {
