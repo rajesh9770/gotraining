32939dc475a42f8f0c3a06f04836a04273a7ac03
rajesh9770 <rajesh_9770@yahoo.com>
2016-09-25 20:14:57 -0700

    closure

---
+++ src/StringUtils/stringutils.go
@@ -0,0 +1,11 @@
+package StringUtils
+
+import (
+
+)
+
+var MyName string = "Rajesh"
+var myName string = "Manjrekar"
+func Concat(a string, b string) string {
+	return a+b
+}
--- src/main/main.go
+++ src/main/main.go
@@ -2,7 +2,17 @@ package main
 
 import (
 	"fmt"
+	"StringUtils"
 )
+
+// return function type
+func wrapper() func() int {
+	 x:=0
+	 return func() int{
+	 	x++
+	 	return x 
+	 }
+}
 
 func main() {
 //	fmt.Println("Hello world Test!")
@@ -15,5 +25,36 @@ func main() {
 	c :=10
 	var f float64
 	
-	fmt.Printf("%v %v %v %v", a, b, c, f)
+	fmt.Printf("%v %v %v %v ", a, b, c, f)
+	
+	fmt.Printf("%v ", StringUtils.Concat("a", "aa"))
+	fmt.Printf("%v ", StringUtils.MyName)
+	fmt.Println("")
+	
+	var x int = 0
+	anonymous := func () int{
+		x++
+		return x
+	}
+	fmt.Printf("%v ", anonymous())
+	fmt.Printf("%v ", anonymous())
+	
+	fmt.Println("")
+	
+	anonymous = wrapper()
+	//anonymous holds on to x which 0 at this point
+	fmt.Printf("%v ", anonymous())
+	fmt.Printf("%v ", anonymous())
+	
+	fmt.Println("")
+	var anonymous1 = wrapper()
+	//anonymous1 holds on to another copy of x which 0 at this point
+	fmt.Printf("%v ", anonymous1())
+	fmt.Printf("%v ", anonymous1())
+	
+	//should be 3
+	fmt.Printf("%v ", anonymous())
 }
+
+
+
